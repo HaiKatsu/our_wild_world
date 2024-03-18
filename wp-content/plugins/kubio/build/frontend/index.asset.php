a27ba5d7d615d29ff5040807c58de2bdf9ce1f76
Ashwin <ashwin@appkorak.com>
2024-03-18 17:43:41 +0000

    [ADD] static pages

--- wp-content/plugins/kubio/build/frontend/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('kubio-scripts', 'wp-polyfill'), 'version' => 'c4f77a9e66736c4d0e9d');
--- wp-content/plugins/kubio/build/icons/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '96493688a8d6041fcf66');
